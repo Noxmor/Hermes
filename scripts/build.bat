5a1a37aaf0d8eea14d12c36ed29b1cd061ee659a
Noxmor <janek.drews@t-online.de>
2023-01-01 17:22:09 +0100

    Added scripts

---
+++ scripts/build.bat
@@ -0,0 +1,32 @@
+@echo off
+SetLocal EnableDelayedExpansion
+
+cd ../
+
+if not exist bin mkdir bin
+if not exist bin-int mkdir bin-int
+
+SET FILES= 
+for /R %%f in (*.c) do (
+	SET FILES=!FILES! %%f
+)
+
+SET FLAGS=-Wall -Wextra -O3
+SET INCLUDES=-I../hermes/src
+SET DEFINES=-DHM_ENABLE_ASSERTS
+
+SET assembly=Hermes
+echo Building %assembly%...
+
+PUSHD bin-int
+gcc %FILES% %FLAGS% -c %INCLUDES% %DEFINES%
+
+SET OBJ_FILES= 
+for /R %%f in (*.o) do (
+	SET OBJ_FILES=!OBJ_FILES! %%f
+)
+POPD
+
+gcc %OBJ_FILES% -o bin/%assembly%.exe %INCLUDES% %DEFINES% %LINKS%
+
+if %ERRORLEVEL% NEQ 0 (exit /b) else (echo Successfully built %assembly%^^!)
