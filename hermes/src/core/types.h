1fa0aed7015ab0a9f5175e69d367ead09577259f
Noxmor <janek.drews@t-online.de>
2023-01-01 17:23:36 +0100

    Added types

---
+++ hermes/src/core/types.h
@@ -0,0 +1,20 @@
+#ifndef HM_TYPES_H
+#define HM_TYPES_H
+
+typedef float f32;
+typedef double f64;
+
+typedef char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef unsigned char b8;
+typedef unsigned int b32;
+
+#endif
