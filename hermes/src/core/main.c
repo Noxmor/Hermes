4f9ff7974bc7985fca953a89a8a5069ad1825c5b
Noxmor <janek.drews@t-online.de>
2023-01-02 23:26:53 +0100

    Added more detailed logging in case of app crash

--- hermes/src/core/main.c
+++ hermes/src/core/main.c
@@ -5,18 +5,24 @@
 
 int main(void)
 {
-	platform_init();
-
 	if (!logger_init())
-		return 1;
-
+		return 1; 
+	
 	HM_INFO("Initialized Log!");
 
+	platform_init();
+	HM_INFO("Initialized platform!");
+
 	Application* app = application_create("Hermes");
+	HM_INFO("Created application!");
 
+	HM_INFO("Running application...");
 	application_run(app);
-
-	logger_shutdown();
+	HM_INFO("Shut down application!");
 
 	platform_shutdown();
+	HM_INFO("Shut down platform");
+
+	HM_INFO("Shutting down log...");
+	logger_shutdown();
 }
