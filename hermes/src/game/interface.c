4179cd550bfc9c04058148d116dd7fb6b3c6277e
Noxmor <janek.drews@t-online.de>
2023-01-30 00:32:40 +0100

    Refactored interface_render_commands() to work with the start pos for rendering

--- hermes/src/game/interface.c
+++ hermes/src/game/interface.c
@@ -2,22 +2,40 @@
 
 #include <string.h>
 
+#include "core/memory_system.h"
+
 #include "platform/platform.h"
 
-void interface_render_commands(Interface* inf, LocaleHandler* locale_handler)
+void interface_render_commands(Interface* inf, u64 x, u64 y, LocaleHandler* locale_handler)
 {
 	for (u8 i = 0; i < inf->command_count; ++i)
 	{
-		Color color = HM_COLOR_FOREGROUND_RED | HM_COLOR_FOREGROUND_GREEN | HM_COLOR_FOREGROUND_BLUE;
-
 		if (i == inf->current_command_index)
 		{
-			color = HM_COLOR_FOREGROUND_RED;
-			platform_set_cursor_pos(2 + strlen(locale_handler_get(locale_handler, inf->commands[i].name_id)), 5 + i);
-			platform_draw_text(0, 5 + i, "> ", color);
-			platform_draw_text(2, 5 + i, locale_handler_get(locale_handler, inf->commands[i].name_id), color);
+			platform_set_cursor_pos(x + 2 + strlen(locale_handler_get(locale_handler, inf->commands[i].name_id)), y + 5 + i);
+			platform_draw_text(x, y + 5 + i, "§e> ");
+
+			char* colored_message = memory_system_malloc((strlen(locale_handler_get(locale_handler, inf->commands[i].name_id)) + 2 + 1) * sizeof(char), HM_MEMORY_GROUP_STRING);
+			strcpy(colored_message, "§e");
+			strcpy(colored_message + 2, locale_handler_get(locale_handler, inf->commands[i].name_id));
+
+			platform_draw_text(x + 2, y + 5 + i, colored_message);
+
+			memory_system_free(colored_message, (strlen(colored_message) + 1) * sizeof(char), HM_MEMORY_GROUP_STRING);
 		}
 		else
-			platform_draw_text(0, 5 + i, locale_handler_get(locale_handler, inf->commands[i].name_id), color);
+			platform_draw_text(x, y + 5 + i, locale_handler_get(locale_handler, inf->commands[i].name_id));
 	}
+}
+
+void interface_move_up(Interface* inf)
+{
+	if (inf->current_command_index > 0)
+		--(inf->current_command_index);
+}
+
+void interface_move_down(Interface* inf)
+{
+	if (inf->current_command_index < inf->command_count - 1)
+		++(inf->current_command_index);
 }
