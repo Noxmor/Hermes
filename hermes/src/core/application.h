40eabcd954850252f756944855ea6770071a4c66
Noxmor <janek.drews@t-online.de>
2023-01-30 00:38:41 +0100

    Added GameState definition and interfaces to header

--- hermes/src/core/application.h
+++ hermes/src/core/application.h
@@ -6,16 +6,48 @@
 #include "game/config.h"
 #include "game/save.h"
 #include "game/locale.h"
+#include "game/interface.h"
 #include "game/location.h"
+
+typedef enum GameState
+{
+	HM_GAMESTATE_MAIN_MENU = 0,
+	HM_GAMESTATE_PLAY_MENU,
+	HM_GAMESTATE_LOCATION_MENU,
+	HM_GAMESTATE_TRAVEL_MENU,
+	HM_GAMESTATE_OPTIONS_MENU,
+	HM_GAMESTATE_LANGUAGE_MENU,
+	HM_GAMESTATE_CONTROLS_MENU,
+	HM_GAMESTATE_BATTLE_MENU,
+	HM_GAMESTATE_DIALOGUE_MENU,
+	HM_GAMESTATE_INVENTORY_MENU,
+	HM_GAMESTATE_QUEST_MENU,
+
+	HM_GAMESTATE_SIZE
+} GameState;
 
 typedef struct Application
 {
 	const char* title;
 	b8 running;
+
 	ConfigHandler* config_handler;
 	Savefile* savefile;
 	LocaleHandler* locale_handler;
 	LocationHandler* location_handler;
+
+	void (*update_func[HM_GAMESTATE_SIZE])(struct Application*);
+
+	Interface main_menu;
+	Interface play_menu;
+	Interface location_menu;
+	Interface travel_menu;
+	Interface options_menu;
+	Interface language_menu;
+	Interface controls_menu;
+	Interface yes_no_menu;
+
+	CommandType current_command_type;
 } Application;
 
 Application* application_create(const char* title);
